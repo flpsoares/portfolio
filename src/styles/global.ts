7141effac22ee0cf08e6c9253fbfcfeaee09ad98
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-14 14:01:41 -0300

    style: added a temporary color palette

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,4 +13,18 @@ export default createGlobalStyle`
     border: 0;
     cursor: pointer;
   }
+
+  html {
+    background: var(--dark);
+    color: var(--text-primary);
+  }
+
+  :root {
+    --dark: #111;
+    --text-primary: #c9d1d9;
+    --primary: #420F56;
+    --secondary: #591F6F;
+    --tertiary: #6F3287;
+    --quaternary: #83459E;
+  }
 `
