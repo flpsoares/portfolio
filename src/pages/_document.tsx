3753eab68fcaffe384746ce118b0f2e2e38a1a74
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-14 16:45:16 -0300

    style: added new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,10 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Exo+2:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
