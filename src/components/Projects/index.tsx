c1db403c37004b95a18c4fe32ff8fe7fcd4a8523
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-21 22:44:21 -0300

    feat: done the project list

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,25 +2,37 @@ import { Project } from '../Project'
 import { ModalProject } from '../ModalProject'
 import { Container, ProjectArea, Title } from './style'
 
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 
 import { ModalContext } from '../../contexts/ModalContext'
 
 import { AnimatePresence } from 'framer-motion'
+import ProjectApi from '../../services/api/ProjectApi'
 
 export const Projects: React.FC = () => {
   const { modalProjectIsOpen } = useContext(ModalContext)
+
+  const [projects, setProjects] = useState<App.Project[]>([])
+
+  useEffect(() => {
+    ProjectApi.list().then((res) => setProjects(Array(res)))
+  }, [])
 
   return (
     <Container>
       <Title>Projetos</Title>
       <AnimatePresence>{modalProjectIsOpen && <ModalProject />}</AnimatePresence>
       <ProjectArea>
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-        <Project />
+        {projects?.map((project, index) => {
+          return (
+            <Project
+              key={project[index].id}
+              name={project[index].name}
+              description={project[index].description}
+              images={project[index].images}
+            />
+          )
+        })}
       </ProjectArea>
     </Container>
   )
