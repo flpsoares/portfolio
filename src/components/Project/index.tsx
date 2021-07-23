f9879951fff4331af8b0d1533b4b3e5cc83c23b0
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-23 19:16:46 -0300

    feat: project component receiving requisition data

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -3,14 +3,21 @@ import { Button, Container, Description, Infos, Title } from './style'
 import Image from 'next/image'
 import { useContext } from 'react'
 import { ModalContext } from '../../contexts/ModalContext'
+import { ProjectContext } from '../../contexts/ProjectContext'
 
 export const Project: React.FC<App.Project> = ({ name, description, images }) => {
   const { openModalProject } = useContext(ModalContext)
+  const { GetProjectInfo } = useContext(ProjectContext)
+
+  const handleClick = () => {
+    openModalProject()
+    GetProjectInfo(name, description, images)
+  }
 
   return (
     <Container>
       <Image
-        src={images[0]?.url ? images[0].url : '/assets/mydiary1.png'}
+        src={images[0].url}
         objectFit="cover"
         alt="mydiary1"
         width={300}
@@ -19,7 +26,7 @@ export const Project: React.FC<App.Project> = ({ name, description, images }) =>
       <Infos>
         <Title>{name}</Title>
         <Description>{description}</Description>
-        <Button onClick={openModalProject}>Ver detalhes</Button>
+        <Button onClick={handleClick}>Ver detalhes</Button>
       </Infos>
     </Container>
   )
