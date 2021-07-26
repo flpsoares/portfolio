4a9682de3ba48e17999a495a3fad26ef2d14acdb
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-26 20:49:53 -0300

    feat: technology component created

---
+++ src/components/Technology/index.tsx
@@ -0,0 +1,20 @@
+import { Container, Title, ImageArea } from './style'
+
+import Image from 'next/image'
+
+interface TechnologyProps {
+  title: string
+  url: string
+  alt: string
+}
+
+export const Technology: React.FC<TechnologyProps> = ({ title, url, alt }) => {
+  return (
+    <Container>
+      <ImageArea>
+        <Image src={url} objectFit="cover" alt={alt} layout="fill" />
+      </ImageArea>
+      <Title>{title}</Title>
+    </Container>
+  )
+}
---
+++ src/components/Technology/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const ImageArea = styled.div`
+  position: absolute;
+
+  width: 150px;
+  height: 150px;
+
+  border-radius: 6px;
+  overflow: hidden;
+`
+
+export const Title = styled.span`
+  font-size: 26px;
+  font-weight: 600;
+
+  margin-top: 150px;
+`
