66114c7cfb31d3b351d6a0c1cf739585b45b86bb
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-18 14:20:27 -0300

    feat: added link and technology data

--- src/contexts/ProjectContext.tsx
+++ src/contexts/ProjectContext.tsx
@@ -2,9 +2,17 @@ import { createContext, ReactNode, useState } from 'react'
 
 interface ProjectContextData {
   name: string
+  link: string | null
   description: string
   images: App.Image[]
-  GetProjectInfo: (name: string, description: string, images: App.Image[]) => void
+  technologies: App.Technology[]
+  GetProjectInfo: (
+    name: string,
+    link: string | null,
+    description: string,
+    images: App.Image[],
+    technologies: App.Technology[]
+  ) => void
 }
 
 interface ProjectProviderProps {
@@ -15,21 +23,29 @@ export const ProjectContext = createContext({} as ProjectContextData)
 
 export function ProjectProvider({ children }: ProjectProviderProps) {
   const [name, setName] = useState('')
+  const [link, setLink] = useState('')
   const [description, setDescription] = useState('')
   const [images, setImages] = useState<App.Image[]>()
+  const [technologies, setTechnologies] = useState<App.Technology[]>()
 
   const GetProjectInfo = (
     name: string,
+    link: string | null,
     description: string,
-    images: App.Image[]
+    images: App.Image[],
+    technologies: App.Technology[]
   ) => {
     setName(name)
+    setLink(link)
     setDescription(description)
     setImages(images)
+    setTechnologies(technologies)
   }
 
   return (
-    <ProjectContext.Provider value={{ name, description, images, GetProjectInfo }}>
+    <ProjectContext.Provider
+      value={{ name, link, description, images, technologies, GetProjectInfo }}
+    >
       {children}
     </ProjectContext.Provider>
   )
