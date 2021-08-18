7e1e7930685cd8836822f3a6ab035990e9ca433e
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-18 14:07:32 -0300

    feat: added more types to project

--- src/@types/project.d.ts
+++ src/@types/project.d.ts
@@ -1,7 +1,9 @@
 declare namespace App {
   export interface Project extends App.Model {
     name: string
+    link?: string | null
     description: string
     images?: App.Image[]
+    technologies?: App.Technology[]
   }
 }
